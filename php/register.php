9b13dc385a6b30637e677aead8850dfc3dca4d35
Cade Dempsey <cadedempsey1@gmail.com>
2024-01-19 12:52:52 -0800

    added sign up form

---
+++ php/register.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+	<form action="register.php" method="POST">
+		<label for="first_name">First Name:</label>
+		<input type="text" id="first_name" name="first_name" required><br><br>
+
+		<label for="last_name">Last Name:</label>
+		<input type="text" id="last_name" name="last_name" required><br><br>
+
+		<label for="email">Email:</label>
+		<input type="email" id="email" name="email" required><br><br>
+
+		<label for="password">Password:</label>
+		<input type="password" id="password" name="password" required><br><br>
+
+		<label for="dob">Date of Birth:</label>
+		<input type="date" id="dob" name="dob" required><br><br>
+		
+	</form>
+	<body> 
+</html>
